c25f4c05632de9be25d6faba0cd3b20e28837b22
plhrja <robert.sirvio@helsinki.fi>
2016-04-24 23:27:42 +0300

    Implemented a script for copying the latest build to the github.io folder with a suffix corredponding to the document language.

---
+++ copy_CV_to_github.io.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Helper script for copying the current cv to the github.io folder.
+
+echo "Starting the copy process..."
+
+# Stop execution on error.
+set -e
+
+DEST="/home/woope/plhrja.github.io/"
+SUFFIX=
+
+# Check for flags.
+while test $# -gt 0; do
+        case "$1" in
+                -en|--english)
+			SUFFIX="en"
+			shift
+                        ;;
+
+		-fin|--finnish)
+			SUFFIX="fi"
+			shift
+                        ;;
+
+                *)
+                        break
+                        ;;
+        esac
+done
+
+if [ $# -ne 1 ]; then
+	echo "Provide a single input file for copying!"
+	exit 1
+fi
+
+if [ -z $SUFFIX ]; then
+	echo "USAGE: copy_CV_to_github.io.sh LANG-FLAG DOCUMENT"
+	echo "	LANG-FLAGS:"
+	echo "		-fin|--finnish: 'fi' suffix"
+	echo "		-en|--english: 'en' suffix"
+	exit 1
+fi
+
+echo "Copying $1 to $DEST with suffix $SUFFIX"
+
+TRG=$(echo $1 | sed "s/.pdf$/_$SUFFIX.pdf/")
+TRG=$DEST$TRG
+cp $1 $TRG
+
+echo "Copying finished!"
